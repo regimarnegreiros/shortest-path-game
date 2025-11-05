9886144a653f8c1217b96dad27cd2bb108df23b6
DanFonR <dannyelfr@yahoo.com.br>
2025-11-05 16:45:37 -0300

    Corrige comparação entre Character e None

--- src/backend/Character.py
+++ src/backend/Character.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from dataclasses import dataclass
+from types import NoneType
 
 @dataclass
 class Character:
@@ -14,15 +15,21 @@ class Character:
         return self.name
 
     def __eq__(self, char: Character | str | int) -> bool:
-        if not isinstance(char, (Character, str, int)):
+        if not isinstance(char, (Character, str, int, NoneType)):
             raise TypeError(f"Unable to compare to {type(char)}")
 
         if isinstance(char, str):
             return char == self.name
         elif isinstance(char, int):
             return char == self.id
+        elif isinstance(char, NoneType):
+            return False
         else:
             return (self.id, self.name) == (char.id, char.name)
 
     def __hash__(self) -> int:
         return hash((self.id, self.name))
+
+if __name__ == "__main__":
+    a = Character(1, "a", ["a"])
+    print(a == None)
