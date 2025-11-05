6d6ff25bd342f060cb5470289f9519e81e83c09d
DanFonR <dannyelfr@yahoo.com.br>
2025-11-05 15:32:20 -0300

    Converte grafo para usar Character ao invés de str

--- src/backend/Character.py
+++ src/backend/Character.py
@@ -7,14 +7,22 @@ class Character:
     name: str
     images: list[str]
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.name
 
-    def __eq__(self, char: Character):
-        if not isinstance(char, Character):
-            raise TypeError("Unable to compare to non-Character")
+    def __eq__(self, char: Character | str | int) -> bool:
+        if not isinstance(char, (Character, str, int)):
+            raise TypeError(f"Unable to compare to {type(char)}")
 
-        return (self.id, self.name) == (char.id, char.name)
+        if isinstance(char, str):
+            return char == self.name
+        elif isinstance(char, int):
+            return char == self.id
+        else:
+            return (self.id, self.name) == (char.id, char.name)
+
+    def __hash__(self) -> int:
+        return hash((self.id, self.name))
